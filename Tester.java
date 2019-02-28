7047d580ebb44fd1b69f3dc968edd91403e05cb1
MusicTheorist <saxophonedude5@gmail.com>
2019-02-27 21:06:35 -0500

    Update Tester.java
    Fixed test array for Grail Sorting without buffer.

--- Tester.java
+++ Tester.java
@@ -56,6 +56,8 @@ public class Tester {
 		}
 		int[] keys = new int[NMaxKey];
 		
+		generateArray(arr, keys, NMax, 0);
+		
 		double timeStart = System.currentTimeMillis();
 		grailSortWithoutBuffer(arr, javaGrailSort);
 		double timeFinish = System.currentTimeMillis();
